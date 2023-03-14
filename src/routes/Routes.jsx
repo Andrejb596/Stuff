dc5050e56ff22ab468a958b64369254dc62ff194
Andrejb596 <andrejb596@gmail.com>
2023-03-14 22:28:58 +0200

    feat: add lazy, Suspense

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,11 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import { About, ErrorPage, Home } from '../pages';
 import { Container } from '../styled/GlobalStyle';
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const About = lazy(() => import('../pages/About/About'));
+const ErrorPage = lazy(() => import('../pages/ErrorPage/ErrorPage'));
 
 export const ROUTES = {
   HOME: '/',
@@ -16,11 +20,13 @@ export default function Routes() {
   return (
     <StyledRoutes>
       <Container>
-        <Switch>
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.ABOUT} element={<About />} />
-          <Route path={ROUTES.ERROR} element={<ErrorPage />} />
-        </Switch>
+        <Suspense fallback={<h2>Loading ...</h2>}>
+          <Switch>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.ABOUT} element={<About />} />
+            <Route path={ROUTES.ERROR} element={<ErrorPage />} />
+          </Switch>
+        </Suspense>
       </Container>
     </StyledRoutes>
   );
